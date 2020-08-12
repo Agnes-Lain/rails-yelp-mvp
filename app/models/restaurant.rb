a8323f5206a595a14ed3ecae47a8b46ec980b265
Yizhu ZONG <agnes.lain@gmail.com>
2020-08-12 15:03:57 +0200

    works with reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,9 @@ class Restaurant < ApplicationRecord
   CATAGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :name, :catagory, :address, presence: true
   validates :catagory, inclusion: { in: CATAGORIES }
+
+  def average_rating
+    sum = reviews.inject(0){|sum,review| sum + review.rating }
+    sum > 0 ? average = (sum/reviews.size).round : average = 0
+  end
 end
